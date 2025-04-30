d3d6738f3c7a31255c793d2dced050d219a6796d
ProjetoAurora-pets <aughustojose5@gmail.com>
2025-04-29 23:23:31 -0300

    Ajustando um erro no código de divisão

--- calculadora.py
+++ calculadora.py
@@ -6,7 +6,7 @@ if operacao == "+":
     num2 = float(input("Digite o segundo número: "))
     resultado = num1 + num2
     print(f"O resultado da soma de {num1} e {num2} é igual a: {resultado}" )
-elif calculadora == "/"
+elif calculadora == "/":
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
     divisao = num1/num2
