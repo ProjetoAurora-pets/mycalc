5ae59895d66349c5140f9ec618afacd6e5b02747
joao victor <joaovctrjvv90@gmail.com>
2025-04-29 22:45:31 -0300

    corrigindo a linha print

--- calculadora.py
+++ calculadora.py
@@ -3,5 +3,5 @@ elif operacao == "*":
     num1 = float(input("Digite o primeiro número: "))
     num2 = float(input("Digite o segundo número: "))
     resultado = num1 * num2
-    print(f"O resultado da soma de {num1} e {num2} é igual a: {resultado}" )
+    print(f"O resultado da multiplicação de {num1} e {num2} é igual a: {resultado}" )
 print("Isto é uma calculadora")
