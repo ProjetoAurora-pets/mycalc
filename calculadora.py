c2c8a67c32ad31e5722cbdde4059e2ba8bfe0c83
Aughusto <aughustojose5@gmail.com>
2025-04-29 22:41:16 -0300

    Estou fazendo um codigo de adicao para uma calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,8 @@
 print("Isto é uma calculadora")
+
+operacao = input("Escolha uma das operações: +|-|*|/|** ")
+if operacao == "+":
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 + num2
+    print(f"O resultado da soma de {num1} e {num2} é igual a: {resultado}" )
