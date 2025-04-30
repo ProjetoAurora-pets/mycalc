f79883ee620088356732890f24811cf2c901a6a7
unknown <joao.jobibo@gmail.com>
2025-04-29 22:45:45 -0300

    versionando codigo de potencia

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,7 @@
 print("Isto é uma calculadora")
+
+elif calculadora == "**":
+    num1 = float(input("Novamente, digite outro numero"))
+    num2 = float(input("Mais outro numero"))
+    resultado = num1 ** num2
+    print(f"O resultado da potencia de {nun1} e {num2} e igual a: {resultado}")
