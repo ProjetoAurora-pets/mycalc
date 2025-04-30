e8fa27baac7bdef0eb364bb851d023b2e54bac62
ProjetoAurora-pets <aughustojose5@gmail.com>
2025-04-29 22:57:30 -0300

    calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,3 @@
-print("Isto é uma calculadora")
-
 #Codigo para um usuario realizar um cálculo de divisão
 
 elif calculadora == "/"
@@ -7,4 +5,3 @@ num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 divisao = num1/num2
 print(f"o resultado da divisão de {num1} por {num2} é: {divisao}")
-
